43724507bd838cddf0f97f3565992247a17d9fca
Matthew Sullivan <matt54633@gmail.com>
2023-02-05 16:16:40 +0000

    Tidy Files

--- files/app.js
+++ files/app.js
@@ -1,7 +1,7 @@
 let information_title = document.getElementById('information_title');
 let week = new Array("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
 let months = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
-
+// display and update time
 function update_clock() {    
     let todays_date = new Date();
     let current_hour = todays_date.getHours();
@@ -15,19 +15,19 @@ function update_clock() {
     information_title.innerText = date_string;
 } 
 setInterval(update_clock, 1000);
-
+// calculate days until birthday and loading bar
 function birthday() {
     let current_date = new Date();
     let current_year = current_date.getFullYear();
     let birthday_date = new Date(current_year, 9, 30);
-    let difference__between_dates = birthday_date.getTime() - current_date.getTime();
-    let days_until_birthday = Math.ceil(difference__between_dates / (1000 * 3600 * 24));
+    let difference_between_dates = birthday_date.getTime() - current_date.getTime();
+    let days_until_birthday = Math.ceil(difference_between_dates / (1000 * 3600 * 24));
     document.getElementById('birthday_para').innerText = days_until_birthday + " days to go!";
     let percentage = 100 - (days_until_birthday / 365 * 100);
     document.getElementById('loading_bar').style.width = percentage + "%";
 }
 birthday();
-
+// load weather data for yeovil in weather widget
 const apiKey = '96ef4b80e6d934610538d335d0ab793e';
 let searchLocation = 'Yeovil';
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchLocation}&appid=${apiKey}&units=metric`)
@@ -38,9 +38,11 @@ fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchLocation}&appid
     document.getElementById('weather_para').innerText = capitaliseConditions(weather[0].description) + ' / Yeovil';
     document.getElementById('weather_img').src = `https://matt54633.com/weather/weatherIcons/${weather[0]["icon"]}.svg`;
 });
-
+// capitalise the weather conditions coming from the api
 function capitaliseConditions(conditionsType) {
     conditionsType = JSON.stringify(conditionsType).replaceAll('"', '');
     conditionsType = conditionsType.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
     return conditionsType;
 }
+// copyright text
+document.getElementById('footer_title').innerHTML = '©Matt Sullivan / ' + new Date().getFullYear();
